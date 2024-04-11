bdf2ef1bd96b22c6d03450888fabe9bc057f8399
JYlag <joeylagan@gmail.com>
2024-04-11 12:35:15 -0700

    finalize

--- cut_version_tag.sh
+++ cut_version_tag.sh
@@ -9,7 +9,6 @@ VERSION=`cat alphafold/version.py | grep version | grep -o "\d*\.\d*\.\d*"`
 CUDA_VERSION=`cat docker/Dockerfile | grep CUDA | ggrep -oP '(?<=CUDA=)\d*(\.\d+)*'`
 OS_VERSION=`cat docker/Dockerfile | grep nvidia/cuda | ggrep -oP 'ubuntu\K[0-9]+\.[0-9]+' | sed 's/^/ubuntu /' | sed 's/ /-/'`
 FULL_TAG="$VERSION-cuda-$CUDA_VERSION-$OS_VERSION"
-echo $FULL_TAG
 
 echo "current git HEAD is \"$(git log --oneline |head -1)\""
 read -p "Would you like to create and push the tag ${FULL_TAG} at the current head of the master branch? (y/n)" proceed
