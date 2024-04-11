233850663e26fc41c768adf75745b0506734664b
JYlag <joeylagan@gmail.com>
2024-04-11 10:56:32 -0700

    finalize PR

--- cut_version_tag.sh
+++ cut_version_tag.sh
@@ -5,7 +5,7 @@ git checkout master
 echo "pulling latest changes"
 git pull
 
-version=`cat alphafold/version.py | grep version | grep -oP "\d+\.\d+\.\d+"`
+version=`cat alphafold/version.py | grep version | grep -o "\d*\.\d*\.\d*"`
 
 echo "current git HEAD is \"$(git log --oneline |head -1)\""
 read -p "Would you like to create and push the tag ${version} at the current head of the master branch? (y/n)" proceed
